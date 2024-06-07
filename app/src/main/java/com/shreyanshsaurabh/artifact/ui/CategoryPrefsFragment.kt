fc413db5b43a458b20d35cce93579ecaa2cd48c0
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-06-07 19:37:17 +0530

    rename - frag

--- app/src/main/java/com/shreyanshsaurabh/artifact/ui/CategoryPrefsFragment.kt
+++ app/src/main/java/com/shreyanshsaurabh/artifact/ui/CategoryPrefsFragment.kt
@@ -8,15 +8,14 @@ import android.view.ViewGroup
 import com.shreyanshsaurabh.artifact.R
 
 
-class PrefsOneFragment : Fragment() {
-
+class CategoryPrefsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prefs_one, container, false)
+        return inflater.inflate(R.layout.fragment_category_prefs, container, false)
     }
 
 
