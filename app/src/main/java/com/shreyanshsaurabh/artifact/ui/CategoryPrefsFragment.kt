cfd3bef9374a66b997b659083403e2668cfe1c64
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-06-10 21:15:20 +0530

    rename - frag

--- app/src/main/java/com/shreyanshsaurabh/artifact/ui/CategoryPrefsFragment.kt
+++ app/src/main/java/com/shreyanshsaurabh/artifact/ui/CategoryPrefsFragment.kt
@@ -10,13 +10,9 @@ import com.shreyanshsaurabh.artifact.R
 
 class CategoryPrefsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_category_prefs, container, false)
     }
-
 
 }
